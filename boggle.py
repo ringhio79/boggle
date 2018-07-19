f6593b27ecdae0e27cbfa7152afce459c6160902
Giselle <gigi108@gmail.com>
2018-07-19 10:22:16 +0000

    initial grid, random letters

---
+++ boggle.py
@@ -0,0 +1,18 @@
+from random import choice
+from string import ascii_uppercase
+
+
+def check():
+    return True
+    
+def make_grid():
+    grid = {}
+    
+    for r in range(2):
+        for c in range(2):
+            grid[r,c] = choice(ascii_uppercase)
+    
+    
+    return grid
+    
+print(make_grid())
